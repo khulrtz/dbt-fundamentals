302cbb86768cfc11247af48ca65ba8abdf41e6c6
khulrtz <jvaldiviaparra@gmail.com>
2022-04-06 15:40:38 +0000

    ATM_UPLOAD

--- models/capa_base/incrementales/T_C1_SAT_PARTSUPP.sql
+++ models/capa_base/incrementales/T_C1_SAT_PARTSUPP.sql
@@ -1,1 +1,1 @@
-{{ mapeos_to_select('dbtfundamentals','STG_PARTSUPP','dbtfundamentals','T_C1_SAT_PARTSUPP') }}
+{{ mapeos_to_select('dbtfundamentals','STG_CALCULADO','dbtfundamentals','T_C1_SAT_PARTSUPP') }}
