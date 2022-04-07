bfbb72095b00e691785dbcb5ac34049fcacd2dda
khulrtz <jvaldiviaparra@gmail.com>
2022-04-07 09:43:51 +0000

    ATM_UPLOAD

--- models/capa_base/incrementales/T_C1_LIK_CUST_NATION.sql
+++ models/capa_base/incrementales/T_C1_LIK_CUST_NATION.sql
@@ -1,1 +1,1 @@
-{{    config(        unique_key='ID'    )}}{{ mapeos_to_select('ingestas','CUSTOMER','dbtfundamentals','T_C1_LIK_CUST_NATION') }}
+{{    config(        unique_key='ID'    )}}{{ mapeos_to_select('ingestas','STG_CUSTOMER','dbtfundamentals','T_C1_LIK_CUST_NATION') }}
